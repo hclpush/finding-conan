4d6638690b82f2ac994e1e70f38406f0416046b9
Yen Shan <yenshan326@gmail.com>
2023-06-03 17:07:11 +0200

    heatmap with the type of crime options

--- frontend/streamlit.py
+++ frontend/streamlit.py
@@ -17,14 +17,14 @@ crime_types = data.columns[17:36]  # Assuming the first column contains dates
 selected_crime_type = st.selectbox('Select a crime type', crime_types)
 
 filtered_df = data[['Date', 'lat','lon',selected_crime_type]]
-selected_date = st.date_input('Select a date')
+
 #Group the incident that happens in the same day
 #grouped_df = data.groupby('Date')[selected_crime_type].sum().reset_index()
 #pivoted_df = grouped_df.pivot_table(index='Date', values=selected_crime_type)
 location_counts = filtered_df.groupby(['lat', 'lon']).size().reset_index(name='Incident Count')
 # Create a density heatmap using Plotly Express
 fig = px.density_mapbox(data, lat=data['lat'], lon=data['lon'], radius= location_counts['Incident Count']*10,  opacity=0.7, color_continuous_scale='YlOrRd',
-                      zoom=10, mapbox_style='carto-positron',animation_frame= selected_date)
+                      zoom=10, mapbox_style='carto-positron',animation_frame = data["Date"] )
 address = st.text_input('Enter an address', '')
 
 st.write('Heatmap of crime incidents')
